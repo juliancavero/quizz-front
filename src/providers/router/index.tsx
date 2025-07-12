b6a1692c6bf8622086e027e754445446cb203b2f
jcaverom <julian.cavero@digio.es>
2025-07-12 20:39:49 +0200

    feat: Simplify routing by removing unused pages and consolidating QuizzPage routes

--- src/providers/router/index.tsx
+++ src/providers/router/index.tsx
@@ -1,20 +1,15 @@
 import NotFound from "@/pages/404";
-import Home from "@/pages/Home";
 import MainFrame from "@/pages/MainFrame";
-import Page3 from "@/pages/Page3";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import QuizzPage from "@/pages/QuizzPage";
-import ResultsPage from "@/pages/ResultsPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainFrame />,
     children: [
-      { index: true, element: <Home /> },
-      { path: "quizz/:quizzId", element: <QuizzPage /> },
-      { path: "results", element: <ResultsPage /> },
-      { path: "page3", element: <Page3 /> },
+      { index: true, element: <QuizzPage /> },
+      { path: ":quizzId", element: <QuizzPage /> },
       { path: "*", element: <NotFound /> },
     ],
   },
